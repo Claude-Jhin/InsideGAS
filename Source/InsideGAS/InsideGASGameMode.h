9b71ec3537165ca3189dc1e4a59a6952a4c7d4d9
Claude-Jhin <76059275+Claude-Jhin@users.noreply.github.com>
2022-11-04 17:48:44 +0800

    Initial commit

---
+++ Source/InsideGAS/InsideGASGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InsideGASGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AInsideGASGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AInsideGASGameMode();
+};
+
+
+
